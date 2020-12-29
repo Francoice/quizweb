6e1ae9fcd73aec4602e3d308055b2950e2bd2bf7
Francoice <71556541+Francoice@users.noreply.github.com>
2020-12-29 20:17:19 +0700

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+// const init = function (){
+//     document.getElementById('button-submit').addEventListener('clik',submit);
+// }
+// const submit = function (ev){
+//     ev.preventDefault ();
+//     ev.stopPropagation();
+//     let ret = validate();
+
+//     if (ret) {
+//         document.getElementById('form-user').submit();
+
+//     }else{
+//         // let err = document.querySelector ('.error');
+//         // let input = err .querySelector ('input');
+//         // err,setAttribute('data-errormsg',....`Missing ${input.placeholder}`);
+//     }
+// }
+
+// const validate = function (ev){
+//     let valid = true;
+//     let chk =document.getElementById('input-hobby');
+//     if (!chk.checked);
+//         valid = false;
+//         chk.parentElement.classList.add('error');
+//     let select = document.getElementById('input-prodi')
+//     let jk = document.getElementById('input-jk')
+//     let nama = document.getElementById('input-nama');
+//     return true;
+// }
+// document.addEventListener('DOMContentLoaded',init);
